a927e198688bb98ed88c1bb9f629de1f236f6bba
iicsx <maxinies00@gmail.com>
2026-06-05 15:54:30 +0200

    update(hypr): adjust workspace script to allow multiple arguments

--- dotfiles/hypr/scripts/hyprland/workspace_action.sh
+++ dotfiles/hypr/scripts/hyprland/workspace_action.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
-hyprctl dispatch "hl.dsp.$1({ $2 = $(((($(hyprctl activeworkspace -j | jq -r .id) - 1)  / 10) * 10 + $3))
-})"
+cmd="$1"
+opt="$2"
+val="$3"
+
+shift 3
+
+args=""
+while [ $# -ge 2 ]; do
+  args+=", $1 = $2"
+  shift 2
+done
+
+hyprctl dispatch "hl.dsp.$cmd({ $opt = $(((($(hyprctl activeworkspace -j | jq -r .id) - 1) / 10) * 10 + $val))${args} })"
